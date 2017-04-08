d8e8377520510e3ee66bf17cd2a3978924a0a29e
kboyko99 <kboyko1999@gmail.com>
2017-04-08 19:50:58 +0300

    week 2 initial commit

---
+++ dataroot/week_2/gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var livereload = require('gulp-livereload');
+
+gulp.task('sass', function () {
+    return gulp.src('./scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+        .pipe(livereload());
+});
+
+gulp.task('sass:watch', function () {
+    livereload.listen();
+    gulp.watch('./scss/**/*.scss', ['sass']);
+});
